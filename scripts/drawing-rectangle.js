e113b8e98c7d488d24be6c7f8b349bc2b687ed41
Midori-Funaki <funamoru@gmail.com>
2017-10-27 14:32:36 +0800

    HTML & JS: Added draw rectangle

--- scripts/drawing-rectangle.js
+++ scripts/drawing-rectangle.js
@@ -6,20 +6,34 @@ class DrawingRectangle extends PaintFunction{
     }
     
     onMouseDown(coord,event){
-        this.contextReal.fillStyle = "#f44";
+        this.contextReal.strokeStyle = "balck";//border color
+        this.contextReal.fillStyle = "#f44";//fill color
+        this.contextReal.lineWidth = "5";//border width
         this.origX = coord[0];
         this.origY = coord[1];
+        
     }
     onDragging(coord,event){
-        this.contextDraft.fillStyle = "#f44";
+        this.contextDraft.strokeStyle = "black";//border color        
+        this.contextDraft.fillStyle = "#f44";//fill color
+        this.contextDraft.lineWidth = "5";//border width
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextDraft.fillRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextDraft.beginPath();
+        this.contextDraft.fillRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY);        
+        this.contextDraft.strokeRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY);        
+        this.contextDraft.fill();
+        this.contextDraft.stroke();
+        this.contextDraft.closePath();
     }
 
     onMouseMove(){}
     onMouseUp(coord){
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextReal.fillRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
+        this.contextReal.beginPath();
+        this.contextReal.rect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY);
+        this.contextReal.fill();
+        this.contextReal.stroke();
+        this.contextReal.closePath();
     }
     onMouseLeave(){}
     onMouseEnter(){}
