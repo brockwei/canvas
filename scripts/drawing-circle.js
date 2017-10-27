a613f4cebd3dd92ee0555fe47978bb404e91eb31
Midori-Funaki <funamoru@gmail.com>
2017-10-27 14:39:34 +0800

    JS:Added circle border

--- scripts/drawing-circle.js
+++ scripts/drawing-circle.js
@@ -6,11 +6,12 @@ class DrawingCircle extends PaintFunction{
     }
     
     onMouseDown(coord,event){
-        this.contextReal.strokeStyle = "#df4b26";
-        this.contextReal.fillStyle = "#df4b26";
-        this.contextDraft.strokeStyle = "gray";
-        this.contextDraft.fillStyle = 'rgba(176,224,230,0.2)'
-        this.contextReal.lineWidth = 5;
+        this.contextReal.strokeStyle = "black";//border color
+        this.contextDraft.strokeStyle = "black";//border color
+        this.contextReal.fillStyle = "#df4b26";//fill color
+        this.contextDraft.fillStyle = "#df4b26";//fill color
+        this.contextReal.lineWidth = 5;//border width
+        this.contextDraft.lineWidth = 5;//border width
         this.origX = coord[0];
         this.origY = coord[1];
         this.contextReal.beginPath();
