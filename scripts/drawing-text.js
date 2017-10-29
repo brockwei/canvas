3a9ec32f12719d7d1aa2c9903187d330aca45d26
Midori-Funaki <funamoru@gmail.com>
2017-10-29 16:17:00 +0800

    JS:New commit of drawing-text

--- scripts/drawing-text.js
+++ scripts/drawing-text.js
@@ -3,41 +3,41 @@ class DrawingText extends PaintFunction{
         super();
         this.contextReal = contextReal;
         this.contextDraft = contextDraft;
-        this.fontWeight = 600;
-        this.fontSize = 35;
-        this.fontStyle = "Arial";
+        this.fontWeight = 600; //font weight
+        this.fontSize = 35; //font size
+        this.fontStyle = "Arial"; //font-family
+        this.fillStyle = "orange"; //font color
         this.textX = [];
         this.textY = [];
     }
     
     onMouseDown(coord,event){
+        //Set up contextReal text properties
         this.contextReal.font = `${this.fontWeight} ${this.fontSize}px ${this.fontStyle}`;
+        this.contextReal.fillStyle = this.fillStyle;
+        //Store the text starting (x,y)
         this.textX.push(coord[0]);
         this.textY.push(coord[1]);
+        //Make the input box appear on the clicked area
         this.fontStartY = this.textY[0] - this.fontSize;
-        console.log('inside if (x,y) '+this.textX[0]+','+this.textY[0]);
-        console.log('fontStartY '+this.fontStartY);
-        $('#textInput').css({"display":"inline-block","top":this.fontStartY,"left":this.textX[0]});
+        $('#textInput').css({"display":"inline-block","top":this.fontStartY,"left":this.textX[0],"font-size":this.fontSize,"color":this.fillStyle,"font-family":this.fontStyle,"font-weight":this.fontWeight,"padding":"0"});
         
+        //If user click outside the input box, text will be printed on the canvas real
+        var keycode = [(event.keyCode ? event.keyCode : event.which)];
+        console.log ('key code '+keycode);
         if ((this.textX.length > 1) && (event.target.id != $('#textInput'))){
             this.outputText(this.contextReal);
         }
-        
     }
 
+    //Print the text on the caval real
     outputText(ctx){
-        console.log('inside outputText function (x,y) '+this.textX[0]+' , '+this.textY[0]);
         let inputText = $('#textInput').val();
-        console.log('input is '+ inputText);
-        contextReal.fillText(inputText,this.textX[0],this.textY[0]);
+        contextReal.fillText(inputText,this.textX[0],this.textY[0]+2);
         contextReal.stroke();
         $('#textInput').css({"display":"none","top":"0","left":"0"});
         $('body').find('input[type=text],input').val('');
         this.textX= [];
         this.textY = [];
-        console.log('after clearing arrays '+this.textX+' , '+this.textY);
     }
-
-
-        
 }
