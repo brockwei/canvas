9741846b890143cb4ce138468a8ee750c5369e34
Brock Wei <weiwei851@gmail.com>
2017-11-01 13:41:29 +0800

    debugging

--- scripts/canvas-common.js
+++ scripts/canvas-common.js
@@ -18,71 +18,73 @@ class PaintFunction{
     onFinish(){}
 }    
 
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $(window).width()<768) {
-    var hammertime = new Hammer(canvasDraft);
-    hammertime.on('drag swipe tap press pan panup pandown', function(ev) {
-    console.log(ev.type);
-    });
-    hammertime.on('panstart',function(ev){
-        let mouseX = ev.center.x - canvasDraft.offsetLeft;
-        let mouseY = ev.center.y - canvasDraft.offsetTop;
-        currentFunction.onMouseDown([mouseX,mouseY],ev);
-        dragging = true;
-        //console.log(mouseX+":"+mouseY + ":"+ev.center.x + ","+ev.center.y);
-    })
-    hammertime.on('panmove',function(ev){
-        let mouseX = ev.center.x - canvasDraft.offsetLeft;
-        let mouseY = ev.center.y - canvasDraft.offsetTop;
-        currentFunction.onDragging([mouseX,mouseY],ev);
-    // currentFunction.onMouseMove([mouseX,mouseY],ev);
-    // console.log("panmove");
-    });
-    hammertime.on('panend',function(ev){
-        let mouseX = ev.center.x - canvasDraft.offsetLeft;
-        let mouseY = ev.center.y - canvasDraft.offsetTop;
-        currentFunction.onMouseUp([mouseX,mouseY],ev);
-    // console.log("panend");
-    
-    });
-}
-
-else if ($(window).width()>767){
-    $('#canvas-draft').mousedown(function(e){
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
+$(window).load(function(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $(window).width()<768) {
+        var hammertime = new Hammer(canvasDraft);
+        hammertime.on('drag swipe tap press pan panup pandown', function(ev) {
+        console.log(ev.type);
+        });
+        hammertime.on('panstart',function(ev){
+            let mouseX = ev.center.x - canvasDraft.offsetLeft;
+            let mouseY = ev.center.y - canvasDraft.offsetTop;
+            currentFunction.onMouseDown([mouseX,mouseY],ev);
+            dragging = true;
+            //console.log(mouseX+":"+mouseY + ":"+ev.center.x + ","+ev.center.y);
+        })
+        hammertime.on('panmove',function(ev){
+            let mouseX = ev.center.x - canvasDraft.offsetLeft;
+            let mouseY = ev.center.y - canvasDraft.offsetTop;
+            currentFunction.onDragging([mouseX,mouseY],ev);
+        // currentFunction.onMouseMove([mouseX,mouseY],ev);
+        // console.log("panmove");
+        });
+        hammertime.on('panend',function(ev){
+            let mouseX = ev.center.x - canvasDraft.offsetLeft;
+            let mouseY = ev.center.y - canvasDraft.offsetTop;
+            currentFunction.onMouseUp([mouseX,mouseY],ev);
+        // console.log("panend");
         
-        currentFunction.onMouseDown([mouseX,mouseY],e);
-        dragging = true;
-    });
-    $('#canvas-draft').mousemove(function(e){
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        if(dragging){
-            currentFunction.onDragging([mouseX,mouseY],e);
-        }
-        currentFunction.onMouseMove([mouseX,mouseY],e);
-    });
-    $('#canvas-draft').mouseup(function(e){
-        dragging = false;
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        currentFunction.onMouseUp([mouseX,mouseY],e);
-    });
-    $('#canvas-draft').mouseleave(function(e){
-    // dragging = false;
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        currentFunction.onMouseLeave([mouseX,mouseY],e);
-    });
-    
-    $('#canvas-draft').mouseenter(function(e){
-        //if(dragging == true){
+        });
+    }
+
+    else if ($(window).width()>767){
+        $('#canvas-draft').mousedown(function(e){
             let mouseX = e.pageX - this.offsetLeft;
             let mouseY = e.pageY - this.offsetTop;
-            currentFunction.onMouseEnter([mouseX,mouseY],e);
-        //}
-    });
-}
+            
+            currentFunction.onMouseDown([mouseX,mouseY],e);
+            dragging = true;
+        });
+        $('#canvas-draft').mousemove(function(e){
+            let mouseX = e.pageX - this.offsetLeft;
+            let mouseY = e.pageY - this.offsetTop;
+            if(dragging){
+                currentFunction.onDragging([mouseX,mouseY],e);
+            }
+            currentFunction.onMouseMove([mouseX,mouseY],e);
+        });
+        $('#canvas-draft').mouseup(function(e){
+            dragging = false;
+            let mouseX = e.pageX - this.offsetLeft;
+            let mouseY = e.pageY - this.offsetTop;
+            currentFunction.onMouseUp([mouseX,mouseY],e);
+        });
+        $('#canvas-draft').mouseleave(function(e){
+        // dragging = false;
+            let mouseX = e.pageX - this.offsetLeft;
+            let mouseY = e.pageY - this.offsetTop;
+            currentFunction.onMouseLeave([mouseX,mouseY],e);
+        });
+        
+        $('#canvas-draft').mouseenter(function(e){
+            //if(dragging == true){
+                let mouseX = e.pageX - this.offsetLeft;
+                let mouseY = e.pageY - this.offsetTop;
+                currentFunction.onMouseEnter([mouseX,mouseY],e);
+            //}
+        });
+    }
+});
 
 $(window).resize(function(){
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $(window).width()<768) {
