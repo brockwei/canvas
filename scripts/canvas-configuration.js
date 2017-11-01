60cefcf8853b2fb48723cdeb784eea2531a06fc8
Brock Wei <weiwei851@gmail.com>
2017-11-01 17:19:48 +0800

    Updated ctrl+z and ctrl+y undo/redo

--- scripts/canvas-configuration.js
+++ scripts/canvas-configuration.js
@@ -30,9 +30,35 @@ var canvasSettings = {
     undoObject: {
         actionCount: 0,
         states: [],
-        savePoint: 0
+        savePoint: 0,
+        undoAction: function(){
+                        if(canvasSettings.undoObject.actionCount>1){
+                            canvasSettings.undoObject.actionCount--;
+                            canvasSettings.undoObject.savePoint = canvasSettings.undoObject.actionCount;
+                            contextReal.drawImage(canvasSettings.undoObject.states[canvasSettings.undoObject.actionCount-1],0,0);
+                        }
+                    },
+        redoAction: function(){
+                        if(canvasSettings.undoObject.actionCount == canvasSettings.undoObject.savePoint && canvasSettings.undoObject.actionCount<canvasSettings.undoObject.states.length){
+                            canvasSettings.undoObject.actionCount++;
+                            canvasSettings.undoObject.savePoint++;
+                            contextReal.drawImage(canvasSettings.undoObject.states[canvasSettings.undoObject.actionCount-1],0,0);
+                        }
+                        else if(canvasSettings.undoObject.actionCount != canvasSettings.undoObject.savePoint){
+                            canvasSettings.undoObject.states.splice(canvasSettings.undoObject.actionCount);
+                            canvasSettings.undoObject.savePoint = canvasSettings.undoObject.actionCount;
+                        }
+                    },
+        //keyPress allows undo&redo to be completed through ctrl+z & ctrl+y
+        keyPress: function(e) {
+            var evtobj = window.event? event : e
+            if (evtobj.keyCode == 90 && evtobj.ctrlKey) {canvasSettings.undoObject.undoAction();}
+            if (evtobj.keyCode == 89 && evtobj.ctrlKey) {canvasSettings.undoObject.redoAction();}
+        }
     }
 }
+
+document.onkeydown = canvasSettings.undoObject.keyPress;
 
 //Change text size
 $("#textSize")[0].oninput = function() {
