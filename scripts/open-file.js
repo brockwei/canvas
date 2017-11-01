656d3f5dbd0c8f80b3c0fc64a0a66c940d43768a
Midori-Funaki <funamoru@gmail.com>
2017-11-01 16:07:00 +0800

    Updated OpenFile function to upload more than 1 images and image middle point draggable

--- scripts/open-file.js
+++ scripts/open-file.js
@@ -18,20 +18,31 @@ class OpenFile extends PaintFunction{
         this.contextReal = ctxReal;
         this.contextDraft = ctxDraft;
         this.actionCount = 0;
-        this.image = img;      
+        this.image = img;
+        this.imageHalfWidth = img.width/2;
+        this.imageHalfHeight = img.height/2;
     }
     onMouseDown(coord){
+        console.log('Image width '+this.imageHalfWidth);
+        console.log('Image height '+this.imageHalfHeight);
+        console.log('Original mouse (x,y) '+coord[0]+' , '+coord[1]);
         dragging = true;
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextDraft.drawImage(this.image,coord[0],coord[1]);
+        this.contextDraft.drawImage(this.image,coord[0]-this.imageHalfWidth,coord[1]-this.imageHalfHeight);
     }
     onDragging(coord){
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextDraft.drawImage(this.image,coord[0],coord[1]);
+        this.contextDraft.drawImage(this.image,coord[0]-this.imageHalfWidth,coord[1]-this.imageHalfHeight);
     }
     onMouseUp(coord){
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.contextReal.drawImage(this.image,coord[0],coord[1]);
+        this.contextReal.drawImage(this.image,coord[0]-this.imageHalfWidth,coord[1]-this.imageHalfHeight);
+        this.onFinish();
         currentFunction = new DrawingFreehand(contextReal,contextDraft);
+    }
+    onFinish(){
+        canvasSettings.undoObject.states[canvasSettings.undoObject.actionCount] = new Image();
+        canvasSettings.undoObject.states[canvasSettings.undoObject.actionCount].src = canvasReal.toDataURL();
+        canvasSettings.undoObject.actionCount++;
     }
 }
